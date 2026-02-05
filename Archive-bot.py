b2b243581e852690a5e0e58902cc217999e013d4
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-02-05 13:08:31 +0800

    Update Archive-bot.py
    Signed-off-by:  臺灣象象 <yellow.elephant.taiwan@gmail.com>

--- Archive-bot.py
+++ Archive-bot.py
@@ -161,14 +161,15 @@ def archive_page(page_name:str, site, archive_page_name:str = "%(page)s/存檔%(
     counter_used = ("%(counter)d" in archive_page_name) and not date_used
     archive_list = {(0, 0) : []}
 
-    new_sections = []
+    new_sections = [sections.header]
     for i in range(threads_num):
         if not re.match("==(?:(?!=)?.*?)?==", sections.sections[i].title.strip()):
             new_sections[-1].content = f"{new_sections[-1].content}\n{sections.sections[i].title}\n{sections.sections[i].content}"
         else:
             new_sections.append(i)
-    threads_num = len(new_sections)
-    sections.sections = new_sections
+    threads_num = len(new_sections - 1)
+    sections.sections = new_sections[1:]
+    sections.header = new_sections[0]
 
     for i in range(threads_num):
         if len(del_list) == maxthreadstoarchive:
